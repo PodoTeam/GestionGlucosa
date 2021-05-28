d8cc051616ad0152c864a060191b7b446b0a1151
toka404 <58258902+toka404@users.noreply.github.com>
2021-05-28 17:51:33 -0500

    Medicamento y properties

---
+++ GlucosaBajoControl/src/glucosabajocontrol/MedicamentoMD.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glucosabajocontrol;
+
+import glucosabajocontrol.ReadProperties;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bryan
+ */
+public class MedicamentoMD {
+
+}
---
+++ GlucosaBajoControl/src/glucosabajocontrol/ReadProperties.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glucosabajocontrol;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ *
+ * @author bryan
+ */
+public class ReadProperties {
+     public String obtenerProperties(String key) throws IOException {
+
+        Properties property = new Properties();
+        String propFileName = "glucosabajocontrol/config.properties";
+
+        //direccion de propiedades
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+        //cargo la direccion
+        if (inputStream != null) {
+            property.load(inputStream);
+        } else {
+            
+            throw new FileNotFoundException("Propierties file no encontrado " + propFileName);
+        }
+        
+        //guardar elemento
+   
+        return property.getProperty(key);
+    }
+    
+    public String obtenerURL()throws IOException
+    {
+        return obtenerProperties("URL");
+    }
+}
