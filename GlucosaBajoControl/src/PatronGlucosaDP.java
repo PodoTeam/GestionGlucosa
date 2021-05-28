255e56acfc072018c31c18c78e64d7f43541b5f7
DanPancho <84941989+DanPancho@users.noreply.github.com>
2021-05-28 18:52:28 -0500

    R.Alimenticia y PatronGlucosa

---
+++ GlucosaBajoControl/src/PatronGlucosaDP.java
@@ -0,0 +1,21 @@
+
+import glucosabajocontrol.GlucosaDP;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author W10USER
+ */
+public class PatronGlucosaDP {
+    GlucosaDP glucosa;
+    public void graficar()
+    {
+        
+    }
+    
+}
