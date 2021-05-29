2cd80b2ea42524fa3cee36055bc9d672c1b9eb4c
DanPancho <84941989+DanPancho@users.noreply.github.com>
2021-05-28 21:21:38 -0500

    GUIS

--- GlucosaBajoControl/src/glucosabajocontrol/GlucosaBajoControl.java
+++ GlucosaBajoControl/src/glucosabajocontrol/GlucosaBajoControl.java
@@ -6,11 +6,14 @@ import java.sql.SQLException;
 public class GlucosaBajoControl {
 
     public static void main(String[] args) throws IOException, SQLException {
-        MedicamentoMD medicamentoMD = new MedicamentoMD();
+        
+        LoginJFrame login = new LoginJFrame();
+        login.setVisible(true);
+        //MedicamentoMD medicamentoMD = new MedicamentoMD();
         //medicamentoMD.insertarMedicamento("aspirina", 100, "10 min antes del almuerzo", true);
-        System.out.println(medicamentoMD.consultarMedicamento().get(0).getIndicaciones());
-        medicamentoMD.modificarMedicamento("aspirina", 100, "20 min antes del almuerzo", true);
-        System.out.println(medicamentoMD.consultarMedicamento().get(0).getIndicaciones());
+       // System.out.println(medicamentoMD.consultarMedicamento().get(0).getIndicaciones());
+        //medicamentoMD.modificarMedicamento("aspirina", 100, "20 min antes del almuerzo", true);
+        //System.out.println(medicamentoMD.consultarMedicamento().get(0).getIndicaciones());
 
     }
 
