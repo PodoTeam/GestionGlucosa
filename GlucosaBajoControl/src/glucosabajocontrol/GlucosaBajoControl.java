16bc6ae53b08aedb81b7710f11b12308abf08b79
toka404 <58258902+toka404@users.noreply.github.com>
2021-05-28 21:32:01 -0500

    Coreccion de conflicto

--- GlucosaBajoControl/src/glucosabajocontrol/GlucosaBajoControl.java
+++ GlucosaBajoControl/src/glucosabajocontrol/GlucosaBajoControl.java
@@ -7,17 +7,9 @@ public class GlucosaBajoControl {
 
     public static void main(String[] args) throws IOException, SQLException {
         
-<<<<<<< HEAD
         LoginJFrame login = new LoginJFrame();
         login.setVisible(true);
-        //MedicamentoMD medicamentoMD = new MedicamentoMD();
-        //medicamentoMD.insertarMedicamento("aspirina", 100, "10 min antes del almuerzo", true);
-       // System.out.println(medicamentoMD.consultarMedicamento().get(0).getIndicaciones());
-        //medicamentoMD.modificarMedicamento("aspirina", 100, "20 min antes del almuerzo", true);
-        //System.out.println(medicamentoMD.consultarMedicamento().get(0).getIndicaciones());
-
-=======
-
+        
     }
 
 }
